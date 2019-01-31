a3870bb62bffcb8e0e58e832efb003d09131beef
gumigumi1117 <t18820yy@sfc.keio.ac.jp>
2019-02-01 03:06:52 +0900

    遅れて本当にすみません。まだ少しうまういかないのであした頑張って出します。本当にすみませんでした

--- final2.js
+++ final2.js
@@ -1,24 +1,9 @@
-function Select() {
-document.getElementsByClassName("keisan").value;
+function Select(obj) {
+//document.getElementById("keisan7");
 
-if(nm1&&nm7 == true){
+if(obj.value=="nm1"||obj.value=="nm16"){
   alert("結果1");
-}else if(nm1&&nm8 == true){
-  alert("結果２");
-}else if(nm1&&nm9.seleced){
-  alert("結果3");
-}else if(nm2.nm10.selected){
-  alert("結果4");
-}else if(nm2.nm5.selected){
-  alert("結果5");
-}else if(nm2.nm6.selected){
-  alert("結果6");
-}else if(nm3.nm4.selected){
-  alert("結果7");
-}else if(nm3.nm5.selecteed){
-  alert("結果8");
-}else if(nm3.nm6.selected){
-  alert("結果9");
+
 }else {
   alert("選択してください");
 }
---
+++ final3.js
@@ -0,0 +1,123 @@
+function Select() {
+var obj1 = document.getElementById("hu");
+var obj2 = document.getElementById("han");
+
+if(obj1.value=="nm1"||obj2.value=="nm16"){
+  alert("結果1");
+} else if(obj1.value=="nm1"||obj2.value=="nm18"){
+  alert("結果1");
+} else if(obj1.value=="nm1"||obj2.value=="nm8"){
+  alert("７００オール");
+} else if(obj1.value=="nm1"||obj2.value=="nm9"){
+  alert("１３００オール");
+} else if(obj1.value=="nm1"||obj2.value=="nm10"){
+  alert("２６００オール");
+} else if(obj1.value=="nm1"||obj2.value=="nm11"){
+  alert("４０００オール");
+} else if(obj1.value=="nm1"||obj2.value=="nm12"){
+  alert("４０００オール");
+} else if(obj1.value=="nm1"||obj2.value=="nm13"){
+  alert("４０００オール");
+} else if(obj1.value=="nm1"||obj2.value=="nm14"){
+  alert("４０００オール");
+} else if(obj1.value=="nm1"||obj2.value=="nm15"){
+  alert("４０００オール");
+} else if(obj1.value=="nm2"||obj2.value=="nm18"){
+  alert("親１５００子１０００");
+} else if(obj1.value=="nm2"||obj2.value=="nm8"){
+  alert("親２９００子２０００");
+} else if(obj1.value=="nm2"||obj2.value=="nm9"){
+  alert("親５８００子３９００");
+} else if(obj1.value=="nm2"||obj2.value=="nm10"){
+  alert("親１１６００子７７００");
+} else if(obj1.value=="nm2"||obj2.value=="nm11"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm2"||obj2.value=="nm12"){
+  alert("親１８０００子１２０００");
+} else if(obj1.value=="nm2"||obj2.value=="nm13"){
+  alert("親２４０００子１６０００");
+} else if(obj1.value=="nm2"||obj2.value=="nm14"){
+  alert("親３６０００子２４０００");
+} else if(obj1.value=="nm2"||obj2.value=="nm15"){
+  alert("親４８０００子３２０００");
+} else if(obj1.value=="nm3"||obj2.value=="nm18"){
+  alert("親７７００子５２００");
+} else if(obj1.value=="nm3"||obj2.value=="nm8"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm3"||obj2.value=="nm9"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm3"||obj2.value=="nm10"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm3"||obj2.value=="nm11"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm3"||obj2.value=="nm12"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm3"||obj2.value=="nm13"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm3"||obj2.value=="nm14"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm3"||obj2.value=="nm15"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm4"||obj2.value=="nm18"){
+  alert("親９６００子６４００");
+} else if(obj1.value=="nm4"||obj2.value=="nm8"){
+  alert("親９６００子６４００");
+} else if(obj1.value=="nm4"||obj2.value=="nm9"){
+  alert("親９６００子６４００");
+} else if(obj1.value=="nm4"||obj2.value=="nm10"){
+  alert("親９６００子６４００");
+} else if(obj1.value=="nm4"||obj2.value=="nm11"){
+  alert("親９６００子６４００");
+} else if(obj1.value=="nm4"||obj2.value=="nm12"){
+  alert("親９６００子６４００");
+} else if(obj1.value=="nm4"||obj2.value=="nm13"){
+  alert("親９６００子６４００");
+} else if(obj1.value=="nm4"||obj2.value=="nm14"){
+  alert("親９６００子６４００");
+} else if(obj1.value=="nm4"||obj2.value=="nm15"){
+  alert("親９６００子６４００");
+} else if(obj1.value=="nm5"||obj2.value=="nm18"){
+  alert("親１１６００子７７００");
+} else if(obj1.value=="nm5"||obj2.value=="nm8"){
+  alert("親１１６００子７７００");
+} else if(obj1.value=="nm5"||obj2.value=="nm9"){
+  alert("親１１６００子７７００");
+} else if(obj1.value=="nm5"||obj2.value=="nm10"){
+  alert("結果1");
+} else if(obj1.value=="nm5"||obj2.value=="nm11"){
+  alert("親１１６００子７７００");
+} else if(obj1.value=="nm5"||obj2.value=="nm12"){
+  alert("親１１６００子７７００");
+} else if(obj1.value=="nm5"||obj2.value=="nm13"){
+  alert("親１１６００子７７００");
+} else if(obj1.value=="nm5"||obj2.value=="nm14"){
+  alert("親１１６００子７７００");
+} else if(obj1.value=="nm5"||obj2.value=="nm15"){
+  alert("親１１６００子７７００");
+} else if(obj1.value=="nm6"||obj2.value=="nm18"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm6"||obj2.value=="nm8"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm6"||obj2.value=="nm9"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm6"||obj2.value=="nm10"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm6"||obj2.value=="nm11"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm6"||obj2.value=="nm12"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm6"||obj2.value=="nm13"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm6"||obj2.value=="nm14"){
+  alert("親１２０００子８０００");
+} else if(obj1.value=="nm6"||obj2.value=="nm15"){
+  alert("親１２０００子８０００");
+
+
+
+
+
+
+
+}
+}
